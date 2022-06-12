4e8bf699bcd4c4f86ae07d13f0e8604fd1fe31f9
Aswerty12 <biboy.janda@gmail.com>
2022-06-12 19:53:20 +0800

    Added Hashing Class

---
+++ Hashing.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace IT123P_Machine_Problem
+{
+    internal class Hashing
+    {
+
+        public string HashString(string entry)
+        {
+            if (String.IsNullOrEmpty(entry))
+            {
+                return String.Empty;
+            }
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(entry);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                // Convert back to a string, removing the '-' that BitConverter adds
+                string hash = BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", String.Empty);
+
+                return hash;
+            }
+            //return processed
+        }
+
+
+
+    }
+}
